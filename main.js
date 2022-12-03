69daf41722d412f60e05745b8a30f4eb044cc0d3
kaliraj <kalisdvk96@gmail.com>
2022-12-03 11:19:57 +0530

    responsive website using html and css

---
+++ main.js
@@ -0,0 +1,12 @@
+$('.tmoni-carousel').owlCarousel({
+    center: true,
+    items:1,
+    loop:true,
+    margin:10,
+	autoplay:true,
+    responsive:{
+        600:{
+            items:1
+        }
+    }
+});
